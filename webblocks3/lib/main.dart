c522ca0a06fb3480ce0cddd80475822bed665004
Thom Hoogmoed <thom.hoogmoed@galileo-academy.nl>
2025-11-12 16:33:41 +0100

    feat: add webblocks3

---
+++ webblocks3/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:webblocks3/Views/webblocks_screen.dart';
+
+void main() {
+  runApp(MaterialApp(home: const WebblocksScreen(), debugShowCheckedModeBanner: false));
+}
